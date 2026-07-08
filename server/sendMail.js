7cf5ffd62a3d685078f3fe4e3883f3aee5b243a3
MFJ-Dev-Coder <m.fischer@sc-p.de>
2026-07-08 08:59:53 +0200

    Update sendMail.js

--- server/sendMail.js
+++ server/sendMail.js
@@ -241,22 +241,26 @@ app.post("/sendMail", upload.any(), async (req, res) => {
       "✅ PDF fertig erstellt"
     );
 
-    const transporter =
-      nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: Number(
-          process.env.SMTP_PORT
-        ),
-        secure: false,
-        auth: {
-          user:
-            process.env.SMTP_USER,
-          pass:
-            process.env.SMTP_PASS
-        }
-      });
+    const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
-    console.log("📧 Sende Mail...");
+console.log("SMTP_HOST:", process.env.SMTP_HOST);
+console.log("SMTP_PORT:", process.env.SMTP_PORT);
+console.log("SMTP_USER:", process.env.SMTP_USER);
+
+console.log("SMTP Verbindung wird geprüft...");
+
+await transporter.verify();
+
+console.log("✅ SMTP Verbindung erfolgreich");
 
     await transporter.sendMail({
       from:
