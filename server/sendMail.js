031e80252712459a8579d5b75f57d19e31c3c06a
MFJ-Dev-Coder <m.fischer@sc-p.de>
2026-07-08 09:37:57 +0200

    Update sendMail.js

--- server/sendMail.js
+++ server/sendMail.js
@@ -244,17 +244,13 @@ app.post("/sendMail", upload.any(), async (req, res) => {
 
     dns.setDefaultResultOrder("ipv4first");
 
- const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
+ cconst transporter = nodemailer.createTransport({
+  host: "outlook.office365.com",
   port: 587,
   secure: false,
   requireTLS: true,
-    
-  family: 4,
 
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  family: 4,
 
   auth: {
     user: process.env.SMTP_USER,
