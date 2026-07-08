23fd1c2369e0311c0e33d095fa7f28e4f2976026
MFJ-Dev-Coder <m.fischer@sc-p.de>
2026-07-08 08:27:42 +0200

    Update sendMail.js

--- server/sendMail.js
+++ server/sendMail.js
@@ -8,6 +8,9 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
