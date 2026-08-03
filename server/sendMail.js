168964f0718f763bde5a083190757189410ab16c
MFJ-Dev-Coder <m.fischer@sc-p.de>
2026-08-03 15:21:44 +0200

    update suche

--- server/sendMail.js
+++ server/sendMail.js
@@ -610,33 +610,16 @@ if (req.files?.length) {
   });
 
 
-        app.get(
-  "/api/device/:internnummer",
-  async (req, res) => {
+    app.get("/api/device/:internnummer", async (req, res) => {
 
-    const { data, error } =
-      await supabase
-        .from("aufnahmen")
-        .select("*")
-        .eq(
-          "internnummer",
-          req.params.internnummer
-        )
-        .order(
-          "erstellt_am",
-          { ascending: false }
-        );
+  const { data, error } =
+    await supabase
+      .from("aufnahmen")
+      .select("*");
 
-    if (error) {
-      return res
-        .status(500)
-        .json(error);
-    }
+  res.json(data);
 
-    res.json(data);
-
-  }
-);
+});
 
 
 const PORT = process.env.PORT || 3001;
